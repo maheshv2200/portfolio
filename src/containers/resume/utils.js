435035424b2980f8704dbae69802fc598304cf12
mahesh <maheshvadla833@gmail.com>
2023-07-21 08:26:39 +0530

    first commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,48 @@
+export const data = {
+    experience: [
+      {
+        title: "Accenture",
+        subTitle: "intern",
+        description:
+          " software developement life cycle",
+        date :"2022",
+      },
+      {
+        title: "I-hub",
+        subTitle: "internship",
+        description:
+          "Web development, Databases, Backend developement",
+          date :"2022",
+      },
+      {
+        title: "itronix solutions",
+        subTitle: "intern",
+        description:
+          "designing user interaction web pages",
+          date :"2023",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Siddhartha E/M high school",
+        description:
+          "10th Grade",
+          date :"2017",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Sharadha junior college",
+        description:
+          "12th Grade in Science",
+          date :"2017-2019",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Mallareddy Institute of Engineering",
+        description:
+          "Bachelor of technology in computer science",
+          date :"2019-2023",
+      },
+    ],
+  };
